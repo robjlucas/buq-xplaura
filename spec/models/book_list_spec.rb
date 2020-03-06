88fc6dcd0d69c0e11d09629aa789f983e61f5598
robjlucas <rob@newleftreview.org>
2020-03-06 10:16:18 +0000

    Validate ISBNs in uploaded book lists

--- spec/models/book_list_spec.rb
+++ spec/models/book_list_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe BookList, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:valid_file) { File.open("spec/support/files/valid_book_list.csv") }
+    let(:invalid_file) { File.open("spec/support/files/invalid_book_list.csv") }
+
+    context "with invalid ISBNs" do
+      it "raises validation errors" do
+        book_list = described_class.create(user: user, file: invalid_file)
+        expect(book_list.errors[:file].first).to include("not a valid ISBN")
+      end
+    end
+
+    context "with valid ISBNs" do
+      it "does not raise validation errors" do
+        book_list = described_class.create(user: user, file: valid_file)
+        expect(book_list.valid?).to be(true)
+      end
+    end
+  end
 end
