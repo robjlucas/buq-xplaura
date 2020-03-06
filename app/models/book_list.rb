749d02f35a328be6520ae5afd8a29f4ecf447bd6
robjlucas <rob@newleftreview.org>
2020-03-06 10:16:18 +0000

    Validate ISBNs in uploaded book lists

--- app/models/book_list.rb
+++ app/models/book_list.rb
@@ -1,7 +1,30 @@
 class BookList < ApplicationRecord
+
+  ISBN10_REGEX = /^(?:\d[\ |-]?){9}[\d|X]$/i.freeze
+  ISBN13_REGEX = /^(?:\d[\ |-]?){13}$/i.freeze
+
   belongs_to :user
 
   self.implicit_order_column = "created_at"
 
   mount_uploader :file, FileUploader
+
+  validates :name, :file, :user_id, presence: true
+  validate :check_isbns
+
+  private def check_isbns
+    if file.read.present?
+      csv = CSV.parse(file.read, headers: true)
+
+      csv.each do |book|
+        unless valid_isbn?(book["isbn"])
+          errors.add(:file, "Validation error for book title '#{book["title"]}': #{book["isbn"]} is not a valid ISBN")
+        end
+      end
+    end
+  end
+
+  private def valid_isbn?(isbn)
+    (isbn&.match(ISBN10_REGEX).present? || isbn&.match(ISBN13_REGEX).present?)
+  end
 end
