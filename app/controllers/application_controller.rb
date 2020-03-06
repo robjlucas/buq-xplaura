8bbd98286dd63cb6b2851326a9e35c9363e8d994
robjlucas <rob@newleftreview.org>
2020-03-06 12:13:41 +0000

    Book list upload UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
+
+  def self.view_accessor(*names)
+    attr_accessor(*names)
+    helper_method(*names)
+  end
 end
--- spec/models/book_list_spec.rb
+++ spec/models/book_list_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe BookList, type: :model do
-  describe "validations" do
-    let(:user) { FactoryBot.create(:user) }
-    let(:valid_file) { File.open("spec/support/files/valid_book_list.csv") }
-    let(:invalid_file) { File.open("spec/support/files/invalid_book_list.csv") }
+  let(:user) { FactoryBot.create(:user) }
+  let(:valid_file) { File.open("spec/support/files/valid_book_list.csv") }
+  let(:invalid_file) { File.open("spec/support/files/invalid_book_list.csv") }
 
+  describe "validations" do
     context "with invalid ISBNs" do
       it "raises validation errors" do
         book_list = described_class.create(user: user, file: invalid_file)
@@ -18,6 +18,13 @@ RSpec.describe BookList, type: :model do
         book_list = described_class.create(user: user, file: valid_file)
         expect(book_list.valid?).to be(true)
       end
+    end
+  end
+
+  describe "file retrieval" do
+    it "returns a parsed CSV file" do
+      book_list = described_class.create(user: user, file: valid_file)
+      expect(user.book_lists.first.csv).to be_a(CSV::Table)
     end
   end
 end
