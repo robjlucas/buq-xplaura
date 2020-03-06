1da68f1fc09d476b27ce242dd91c06fa09121e0a
robjlucas <rob@newleftreview.org>
2020-03-06 08:31:01 +0000

    AWS uploader to store files named by UUID IDs

---
+++ app/uploaders/file_uploader.rb
@@ -0,0 +1,11 @@
+class FileUploader < CarrierWave::Uploader::Base
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/"
+  end
+
+  def filename
+    model.id.to_s
+  end
+end
