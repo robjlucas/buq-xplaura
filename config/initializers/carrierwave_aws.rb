1484bdfd575b87014732eb49ba2683d536ec9f16
robjlucas <rob@newleftreview.org>
2020-03-10 09:10:56 +0000

    fixup! Install and configure Carrierwave AWS gem

--- config/initializers/carrierwave_aws.rb
+++ config/initializers/carrierwave_aws.rb
@@ -4,14 +4,13 @@ CarrierWave.configure do |config|
     config.enable_processing = false
   else
     config.storage    = :aws
-    config.aws_bucket = ENV.fetch('S3_BUCKET_NAME') # for AWS-side bucket access permissions config, see section below
+    config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
     config.aws_acl    = 'private'
 
     config.aws_credentials = {
       access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      region:            ENV.fetch('AWS_REGION'), # Required
-      stub_responses:    Rails.env.test? # Optional, avoid hitting S3 actual during tests
+      region:            ENV.fetch('AWS_REGION')
     }
   end
 end
